70caf3b5d98f48469e2270c00d8327a0c1290d63
Jie Pu <i@jaypu.com>
2022-07-29 21:52:33 +0800

    update

--- emulators/RetroArch/Android/RetroArch.Full/replace_sd_id_in_playlists.sh
+++ emulators/RetroArch/Android/RetroArch.Full/replace_sd_id_in_playlists.sh
@@ -1,3 +1,5 @@
 # replace 9C33-6BBD to your SD card ID.
 # should using gnu sed, macOS sed is not working
 sed -i "s/XXXX-XXXX/9C33-6BBD/g" `grep XXXX-XXXX -rl ./`
+# or
+find ./ -type f -exec sed -i -e 's/XXXX-XXXX/9C33-6BBD/g' {} \;
