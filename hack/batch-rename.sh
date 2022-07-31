c4a9f86475f74008e8b9ec7a4bbc301c31b46ead
Jie Pu <i@jaypu.com>
2022-07-31 19:01:21 +0800

    rename

---
+++ hack/batch-rename.sh
@@ -0,0 +1,14 @@
+
+# 动态传入参数的写法比较复杂，如下动态生成命令的方式比较快速简单
+# rename 's/3DO/3DO\ -\ 3DO/' */*/*.png
+# rename 's/CPS1/Capcom\ -\ CPS1/' */*/*.png
+# rename 's/CPS2/Capcom\ -\ CPS2/' */*/*.png
+# rename 's/DOS/Microsoft\ -\ DOS/' */*/*.png
+# rename 's/NeoGeo/SNK\ -\ NeoGeo/' */*/*.png
+# rename 's/PCE/NEC\ -\ PCE/' */*/*.png
+# rename 's/PPSSPP/Sony\ -\ PSP/' */*/*.png
+# rename 's/PS1/Sony\ -\ PS1/' */*/*.png
+# rename 's/PS2/Sony\ -\ PS2/' */*/*.png
+
+rename 's/Capcom\ -\ CSony\ -\ PS1/Capcom\ -\ CPS1/' */*/*.png
+rename 's/Capcom\ -\ CSony\ -\ PS2/Capcom\ -\ CPS2/' */*/*.png
